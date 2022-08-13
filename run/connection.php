a253008172675dbe45d5fda7e81175c6eab2c8bd
Natãn Schneider <natanschneider@protonmail.com>
2022-08-13 17:51:00 -0300

    *bug-fix* database URL

--- run/connection.php
+++ run/connection.php
@@ -1,8 +1,12 @@
 <?php
     class BD{
-        public function ConectarBanco(){    
-            $con_Bd = mysqli_connect ('localhost','root','','micro_classificacao');
-            
+        public function ConectarBanco(){
+            $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+            if($url == 'http://localhost'){
+                $con_Bd = mysqli_connect ('localhost','root','','micro_classificacao');
+            }elseif($url == 'https://tabelabrasileiroseriea.000webhostapp.com'){
+                $con_Bd = mysqli_connect ('localhost','id19376755_root','gUm#/FaR#66}nae%','micro_classificacao');
+            }
             return $con_Bd;
         }
     }
