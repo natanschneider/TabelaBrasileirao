c28998add2dec8ace6af3fbb005691c1ff8ecec5
Natãn Schneider <natanschneider@protonmail.com>
2022-08-13 18:43:38 -0300

    documentation inside file

--- run/connection.php
+++ run/connection.php
@@ -2,10 +2,10 @@
     class BD{
         public function ConectarBanco(){
             $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-            if($url == 'http://localhost'){
+            if($url == 'http://localhost'){ //use esse if para conectar com o MySQL do xampp
                 $con_Bd = mysqli_connect ('localhost','root','','micro_classificacao');
-            }elseif($url == 'https://tabelabrasileiroseriea.000webhostapp.com'){
-                $con_Bd = mysqli_connect ('localhost','usuario','senha','micro_classificacao');
+            }elseif($url == 'URL da pagina após deploy para hospedagem'){
+                $con_Bd = mysqli_connect ('hostname','usuario','senha','database');
             }
             return $con_Bd;
         }
