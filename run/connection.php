09bd80440f6acee1720c76eaaec07125007dca82
Natãn Schneider <natanschneider@protonmail.com>
2022-08-20 05:42:08 -0300

    *bug-fix*

--- run/connection.php
+++ run/connection.php
@@ -5,7 +5,7 @@
             if($url == 'http://localhost'){
                 $con_Bd = mysqli_connect ('localhost','root','','micro_classificacao');
             }elseif($url == 'https://tabelabrasileiroseriea.000webhostapp.com'){
-                $con_Bd = mysqli_connect ('localhost','id4534567_root','gUm#/3825fgbnae%','id1933750295_micro_classificacao');
+                $con_Bd = mysqli_connect ('localhost','id49876965534567_root','gUm#/38264nfg5fgbnae%','id193375345760295_micro_classificacao');
             }
             return $con_Bd;
         }
