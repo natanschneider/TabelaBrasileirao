fa4510d6f6a472cf22151fe08d40017a53bb4156
Natãn Schneider <natanschneider@protonmail.com>
2022-08-20 00:37:02 -0300

    *feature* file creation

---
+++ run/log.php
@@ -0,0 +1,11 @@
+<?php
+    class log{
+        public function create(){
+            $curDate = date('Y-m-d');
+            $logDate = date('Y_m_d-H-i');
+
+            $arquivo = 'log_'.$logDate.'.txt';
+
+            $ = fopen();
+        }
+    }
